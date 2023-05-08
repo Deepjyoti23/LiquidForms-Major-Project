35d480de622d2ebf93153e5c345f92d3390d7234
Deepjyoti23 <deepjyoti116@gmail.com>
2023-05-08 15:05:30 +0530

    ..

---
+++ backend/routers/formRouter.js
@@ -0,0 +1,44 @@
+const req = require('express/lib/request');
+const Model = require('../models/formModel');
+const {Router} = require('express');
+const router = Router();
+
+router.post('/add', (req,res) => {
+    console.log(req.body);
+    
+    new Model(req.body).save()
+    .then((result) => {
+        
+        res.json(result);
+
+    }).catch((err) => {
+
+        console.error(err);
+        res.status(500).json(err);
+        
+    });
+});
+    router.post('/authenticate', (req, res) => {
+
+        Model.findOne(req.body)
+        .then((result) => {
+            if(result) res.json(result);
+            else res.status(401).json({message : 'Invalid Credentials' });
+        }).catch((err) => {
+            console.error(err);
+            res.status(500).json(err);
+        });
+
+    });
+    
+    router.get('/getall', (req,res) => {
+        Model.find({})
+        .then((result) => {
+            res.json(result);
+        }).catch((err) => {
+            console.error(err);
+            req.status(500).json(err);
+        });    
+    })
+
+module.exports  = router;
